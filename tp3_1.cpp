a3ad67def428df30bd5a0e43b00f691ab02df0b5
Ramos-Gabriel <ramogsantana@outlook.com.ar>
2019-03-28 18:46:45 -0300

    archivo base

---
+++ tp3_1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+typedef struct {
+TRaza Raza; //nota 1
+Char *ApellidoNombre; //nota 2
+int edad; //entre 0 a 300
+double Salud://100
+}TDatos;
+
+
+int main(){
+	
+
+
+}
